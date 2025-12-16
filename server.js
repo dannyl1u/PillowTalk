b4abcfd85f4ce156008043bc4e49d9f01b4c5475
dannyl1u <dannyjialiliu@gmail.com>
2025-12-15 18:12:51 -0800

    wip

--- server.js
+++ server.js
@@ -239,7 +239,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 server.listen(PORT, () => {
   console.log(`
 ╔════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗
